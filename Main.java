1d8a0ab34a85c2b2302c9697f1dede092f1d0c09
Seisuke Inamine <e225225@ie.u-ryukyu.ac.jp>
2025-02-21 18:36:37 +0900

    1st commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = null;  // null を代入
+        System.out.println(str.length());  //  NullPointerException発生
+    }
+}
